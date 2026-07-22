62fccd35a3de4daa96a471dd169137d1e3b132bd
Lakshmi Deepak Kumar <lakshmideepakkumarlucky@gmail.com>
2026-07-23 00:01:41 +0530

    Time: 0 ms (100%), Space: 8.9 MB (35.37%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.length() - 1;
+        int len = 0;
+
+        // Skip trailing spaces
+        while (i >= 0 && s[i] == ' ')
+            i--;
+
+        // Count the last word
+        while (i >= 0 && s[i] != ' ') {
+            len++;
+            i--;
+        }
+
+        return len;
+    }
+};
